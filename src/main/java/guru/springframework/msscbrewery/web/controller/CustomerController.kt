0f526183a17f7a9ddfcddfa3e35ff97823fd9502
ivanxalie <ivanxalie@gmail.com>
2024-03-25 20:34:32 +0300

    Change Customer to CustomerDto

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.kt
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.kt
@@ -3,7 +3,7 @@ package guru.springframework.msscbrewery.web.controller
 import guru.springframework.msscbrewery.config.AppConfig
 import guru.springframework.msscbrewery.config.AppConfig.Companion.API_CUSTOMER_V1_PATH
 import guru.springframework.msscbrewery.services.CustomerService
-import guru.springframework.msscbrewery.web.model.Customer
+import guru.springframework.msscbrewery.web.model.CustomerDto
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -15,12 +15,12 @@ import java.util.*
 class CustomerController(private val customerService: CustomerService) {
 
     @GetMapping("/{customerId}")
-    fun getCustomer(@PathVariable("customerId") customerId: UUID): ResponseEntity<Customer> =
+    fun getCustomer(@PathVariable("customerId") customerId: UUID): ResponseEntity<CustomerDto> =
         ResponseEntity.ok(customerService.getById(customerId))
 
     @PostMapping
-    fun addCustomer(@RequestBody customer: Customer): ResponseEntity<Any> {
-        val saved = customerService.saveNewCustomer(customer)
+    fun addCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<Any> {
+        val saved = customerService.saveNewCustomer(customerDto)
         val headers = HttpHeaders()
         headers.add("Location", "${AppConfig.MOCK_HOST_PORT}/$API_CUSTOMER_V1_PATH/${saved.id}")
         return ResponseEntity(headers, HttpStatus.CREATED)
@@ -28,10 +28,10 @@ class CustomerController(private val customerService: CustomerService) {
 
     @PutMapping("/{customerId}")
     fun updateCustomer(
-        @RequestBody customer: Customer,
+        @RequestBody customerDto: CustomerDto,
         @PathVariable("customerId") customerId: UUID,
     ): ResponseEntity<Any> {
-        customerService.updateCustomer(customerId, customer)
+        customerService.updateCustomer(customerId, customerDto)
         return ResponseEntity.noContent().build()
     }
 
